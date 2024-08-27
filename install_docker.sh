82728539b92593591572e3d4ad3b911355fe6f60
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-08-27 15:23:04 -0500

    refactor: abstract tasks into functions
    error handling, formatting, etc.

--- install_docker.sh
+++ install_docker.sh
@@ -1,67 +1,103 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# trap signals 0, 2, 3, 15
+trap 'exit' EXIT SIGINT SIGQUIT SIGTERM
 
 # Ensure the script is run as root
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+	echo "This script must be run as root" 1>&2
+	exit 1
 fi
 
+check_docker_group() {
+	if getent group docker | grep -q "\b$SUDO_USER\b"; then
+		echo "User is already in the docker group."
+		return 0
+	fi
+}
+
 add_user_to_docker_group() {
-    grep -E '^docker:' /usr/lib/group | tee -a /etc/group > /dev/null && usermod -aG docker $SUDO_USER
-    echo "User added to the docker group. Please reboot the system to apply changes."
-    read -p "Press Enter to reboot or CTRL+C to cancel..."
-    reboot
+	if check_docker_group; then
+		return 0
+	fi
+	usermod -aG docker "$SUDO_USER"
+	echo "User added to the docker group. "
+	read -p "Press Enter to activate the changes or CTRL+C to cancel..."
+	newgrp docker
+}
+
+check_docker_service() {
+	if systemctl is-active --quiet docker; then
+		echo "Docker service is already running."
+		return 0
+	fi
 }
 
 ask_enable_docker_service() {
-    echo "Would you like to start and enable Docker? [y/N]"
-    read -r response
-    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        systemctl start docker
-        systemctl enable docker
-        echo "Docker service started and enabled."
-    else
-        echo "Skipping Docker service start and enable."
-    fi
+	if ! check_docker_service; then
+		echo "Would you like to start and enable Docker? [y/N]"
+		read -r response
+		if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+			systemctl enable --now docker
+			echo "Docker service started and enabled."
+		else
+			echo "Skipping Docker service start and enable."
+		fi
+	fi
 }
 
+add_docker_repo() {
+	if [[ ! -f "/etc/yum.repos.d/docker.repo" ]]; then
+		tee -a /etc/yum.repos.d/docker.repo <<-EOF
+		[docker-ce-stable]
+		name=Docker CE Stable - \$basearch
+		baseurl=https://download.docker.com/linux/fedora/\$releasever/\$basearch/stable
+		enabled=1
+		gpgcheck=1
+		gpgkey=https://download.docker.com/linux/fedora/gpg
 
-# Check if Docker is installed
-if command -v docker &> /dev/null; then
-    echo "Docker is already installed. Proceeding to add user to Docker group..."
-    add_user_to_docker_group
-    ask_enable_docker_service
-else
-    # Add Docker CE repository
-    cat <<EOF > /etc/yum.repos.d/docker.repo
-[docker-ce-stable]
-name=Docker CE Stable - \$basearch
-baseurl=https://download.docker.com/linux/fedora/\$releasever/\$basearch/stable
-enabled=1
-gpgcheck=1
-gpgkey=https://download.docker.com/linux/fedora/gpg
+		[docker-ce-stable-debuginfo]
+		name=Docker CE Stable - Debuginfo \$basearch
+		baseurl=https://download.docker.com/linux/fedora/\$releasever/debug-\$basearch/stable
+		enabled=0
+		gpgcheck=1
+		gpgkey=https://download.docker.com/linux/fedora/gpg
 
-[docker-ce-stable-debuginfo]
-name=Docker CE Stable - Debuginfo \$basearch
-baseurl=https://download.docker.com/linux/fedora/\$releasever/debug-\$basearch/stable
-enabled=0
-gpgcheck=1
-gpgkey=https://download.docker.com/linux/fedora/gpg
+		[docker-ce-stable-source]
+		name=Docker CE Stable - Sources
+		baseurl=https://download.docker.com/linux/fedora/\$releasever/source/stable
+		enabled=0
+		gpgcheck=1
+		gpgkey=https://download.docker.com/linux/fedora/gpg
+		EOF
+	fi
+}
 
-[docker-ce-stable-source]
-name=Docker CE Stable - Sources
-baseurl=https://download.docker.com/linux/fedora/\$releasever/source/stable
-enabled=0
-gpgcheck=1
-gpgkey=https://download.docker.com/linux/fedora/gpg
-EOF
+check_docker_install() {
+	if rpm-ostree status | grep -q "docker"; then
+		echo "Docker is already installed."
+		ask_enable_docker_service
+		add_user_to_docker_group
+		exit 0
+	fi
+}
 
-    # Install Docker packages
-    rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+install_docker() {
+	rpm-ostree install \
+		docker-ce \
+		docker-ce-cli \
+		containerd.io \
+		docker-buildx-plugin \
+		docker-compose-plugin
 
-    echo "Docker packages installed. The system will now reboot to apply changes."
-    read -p "Press Enter to continue with the reboot or CTRL+C to cancel..."
-    reboot
+	echo "Docker packages installed. The system will now reboot to apply changes."
+	read -p "Press Enter to continue with the reboot or CTRL+C to cancel..."
+	reboot
+}
 
-    # After reboot, the script needs to be run again manually to add the user to the Docker group.
-fi
+main() {
+	check_docker_install
+	add_docker_repo
+	install_docker
+}
+main
